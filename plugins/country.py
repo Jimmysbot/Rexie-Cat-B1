0d9e083a617621dee8db0a7fbea0082c30c3f250
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-11 21:54:51 +0530

    Update country.py

--- plugins/country.py
+++ plugins/country.py
@@ -1,4 +1,4 @@
-from config import *
+from config import
 from countryinfo import CountryInfo
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
