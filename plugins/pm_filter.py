2dd0da1eeeeef8de3159713020f3e1e26af4b0bd
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-11 20:57:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -524,16 +524,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-
     elif query_data == "countryinfo": 
         buttons = [[
             InlineKeyboardButton('◀️ back', callback_data='home'),
             InlineKeyboardButton('🏘 home', callback_data='start')
         ]]
-        
         reply_markup = InlineKeyboardMarkup(buttons)
-        
-        await update.message.edit_text(
+        await query.message.edit_text(
             COUNTRY_TEXT,
             reply_markup=reply_markup,
             parse_mode="html"
