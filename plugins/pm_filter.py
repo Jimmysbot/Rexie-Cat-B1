e8a3cf775312d891d6785254fa895b5861c3dbb6
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-11 21:04:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -531,7 +531,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            COUNTRY_TEXT,
+            text=COUNTRY_TEXT,
             reply_markup=reply_markup,
             parse_mode="html"
         )
