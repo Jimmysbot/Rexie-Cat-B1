58bf1100de19d0f19cba886e34f5f221fcf57110
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 13:02:28 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -38,3 +38,8 @@ async def cheers(bot, message):
 async def rip(bot, message):
          chat_id=message.chat.id
          await bot.send_dice(chat_id=chat_id, emoji='⚰️')
+
+@Client.on_message(filters.command(["thunder"]))
+async def thunder(bot, message):
+         chat_id=message.chat.id
+         await bot.send_dice(chat_id=chat_id, emoji='⚡')
