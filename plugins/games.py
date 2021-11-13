5472adc085b3db55f48a2fbbc7616b51a152b96d
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 13:04:31 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -48,3 +48,8 @@ async def thunder(bot, message):
 async def happybirthday(bot, message):
          chat_id=message.chat.id
          await bot.send_dice(chat_id=chat_id, emoji='🎊')
+
+@Client.on_message(filters.command(["vgame"]))
+async def vgame(bot, message):
+         chat_id=message.chat.id
+         await bot.send_dice(chat_id=chat_id, emoji='🎮')
