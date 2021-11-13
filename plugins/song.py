05ca21ffc29a7e61139b6b3053e3729adee914ee
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 09:18:56 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -68,7 +68,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Requested By:</b> {message.from_user.mention()} \n📤 <b>Uploaded By: [REXIE](https://t.me/joinchat/09UWqAo8tH9iNmE9)</b> 👑'
+        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Requested By:</b> {message.from_user.mention()} \n📤 <b>Uploaded By: @Rexie_Support</b> 👑'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -77,7 +77,7 @@ def a(client, message):
         m.delete()
         message.delete()
     except Exception as e:
-        m.edit('**An Error Occured. Please Report This To [REXIE SUPPORT](https://t.me/joinchat/09UWqAo8tH9iNmE9) !!**')
+        m.edit('**An Error Occured. Please Report This To @Rexie_Support !!**')
         print(e)
     try:
         os.remove(audio_file)
