0fdee4ba61400f50c24f768ee7c441f3866d83cf
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 10:47:16 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -1,9 +1,19 @@
 from pyrogram import Client, filters
 
-# Sticke
-@Client.on_message(filters.command(["sticke"]))
-async def sticke(bot, message):   
-    if message.reply_to_message.("sticke"):
-       await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
-    else: 
-       await message.reply("Oops !! Not a sticker file")
+@Client.on_message(filters.command("Hyy"))
+async def reply_info(client, message):
+    query = message.text.split(None, 1)[1]
+    reply_markup = BUTTONS
+    await message.reply_text(
+        text=stick_snd(query),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=reply_markup
+    )
+def stick_snd(country_name):
+    try:
+stick_snd = f"""Hey how are you"""
+
+return stick_snd
+    except Exception as error:
+        return error
