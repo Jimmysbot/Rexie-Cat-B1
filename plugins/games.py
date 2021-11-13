24e8380ac5c1d7b466178cc2e789dcee00ce71b1
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 11:45:16 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -1,4 +1,4 @@
 from pyrogram import Client, filters
 @Client.on_message(filters.command(["football"]))
 async def football(bot, message):
-         await bot.update.reply_sticker("CAACAgUAAxkBAAIFKmDd2r4NMyGSyWgVu2v-fQxvJxBxAAL1AgACufE4VgHHxPJeyWOKHgQ")
+         await update.reply_sticker("CAACAgUAAxkBAAIFKmDd2r4NMyGSyWgVu2v-fQxvJxBxAAL1AgACufE4VgHHxPJeyWOKHgQ")
