38c5bc1e165533e278f75adde668c667c0af9e5b
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 12:17:07 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, filters
 @Client.on_message(filters.command(["football"]))
 async def football(bot, message):
-         await bot.send_dice(
-      emoji="⚽️")
+         await bot.send_dice(emoji="⚽️")
