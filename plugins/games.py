a1a1f89c9d466bcdcefe90fe9ed8ef659d5dbb24
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 10:59:39 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -1,19 +1,3 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("Hyy"))
-async def reply_info(client, message):
-    query = message.text.split(None, 1)[1]
-    reply_markup = BUTTONS
-    await message.reply_text(
-        text=stick_snd(query),
-        disable_web_page_preview=True,
-        quote=True,
-        reply_markup=reply_markup
-    )
-def stick_snd(Hyy_ab):
-    try:
-stick_snd = f"""Hey how are you"""
-
-return stick_snd
-    except Exception as error:
-        return error
+@Clinet.on_message(filters.private & filter.command("football"))
+async def (bot, message):
+         await bot.send_sticker(chat_id,"CAACAgUAAxkBAAIFKmDd2r4NMyGSyWgVu2v-fQxvJxBxAAL1AgACufE4VgHHxPJeyWOKHgQ")
