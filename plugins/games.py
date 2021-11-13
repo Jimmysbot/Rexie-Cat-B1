d0504f43c5750dc3c4c2e438ef4fe6fe0322befb
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 11:01:39 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -1,3 +1,4 @@
+from pyrogram import Client, filters
 @Clinet.on_message(filters.private & filter.command("football"))
 async def (bot, message):
-         await bot.send_sticker(chat_id,"CAACAgUAAxkBAAIFKmDd2r4NMyGSyWgVu2v-fQxvJxBxAAL1AgACufE4VgHHxPJeyWOKHgQ")
+         await bot.send_sticker("CAACAgUAAxkBAAIFKmDd2r4NMyGSyWgVu2v-fQxvJxBxAAL1AgACufE4VgHHxPJeyWOKHgQ")
