b34c42587f6fa52d17e9c1f378444522bb3815da
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 10:24:04 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 # Sticke
 @Client.on_message(filters.command(["sticke"]))
 async def sticke(bot, message):   
-    if message.reply_to_message.sticke:
+    if message.reply_to_message.["sticke"]:
        await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
     else: 
        await message.reply("Oops !! Not a sticker file")
