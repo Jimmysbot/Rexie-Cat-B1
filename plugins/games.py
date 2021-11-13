fdae3903de2b3a5d0de01a3c28aeae94eed09a2a
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-13 11:06:56 +0530

    Update games.py

--- plugins/games.py
+++ plugins/games.py
@@ -1,4 +1,4 @@
 from pyrogram import Client, filters
 @Clinet.on_message(filters.private & filter.command("football"))
-async def (bot, message):
+async def football(bot, message):
          await bot.send_sticker("CAACAgUAAxkBAAIFKmDd2r4NMyGSyWgVu2v-fQxvJxBxAAL1AgACufE4VgHHxPJeyWOKHgQ")
