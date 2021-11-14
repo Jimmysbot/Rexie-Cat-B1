b93eddc660cf3b07dfcf2f634fea050ca642aa7d
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-14 16:13:37 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -11,4 +11,5 @@ async def welcome(bot,message):
 async def goodbye(bot,message):
 	chatid= message.chat.id
 	await bot.send_message(text=f"**Bye ,  {message.from_user.mention} , Have a Nice Day**",chat_id=chatid)
-	
+await asyncio.sleep(600)
+await m.delete	
