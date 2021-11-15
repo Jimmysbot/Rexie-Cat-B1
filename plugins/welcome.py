6dd8cfcf9aa4ff77084ef95c9651a900c81022bc
Shiva <90966061+Shiva123kphkph@users.noreply.github.com>
2021-11-15 14:56:26 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -5,13 +5,13 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-        msg = await bot.send_message(text=f"😍 Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
-await asyncio.sleep(10)
-await msg.delete()	
+  msg = await bot.send_message(text=f"😍 Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+        await asyncio.sleep(10)
+        await msg.delete()	
 
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
 	chatid= message.chat.id
-        msg = await bot.send_message(text=f"**Bye ,  {message.from_user.mention} , Have a Nice Day**",chat_id=chatid)
-await asyncio.sleep(10)
-await msg.delete()
+  msg = await bot.send_message(text=f"**Bye ,  {message.from_user.mention} , Have a Nice Day**",chat_id=chatid)
+        await asyncio.sleep(10)
+        await msg.delete()
